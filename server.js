6204938ab8a1c13de1c99e8d19925bd048b38cac
Qudratullah Khurram <qudrat.khurram@gmail.com>
2025-08-27 23:16:35 +0100

    my  intro-to-express-lab assignment

--- server.js
+++ server.js
@@ -52,3 +52,43 @@ app.get ("/hello", (req, res) => {
     const age = req.params.age;
   res.send(`Hello there, ${req.query.name}! I hear you are ${req.query.age} years old!`)
 });
+
+////////////////////////////////////////////////////////////////
+
+ 
+
+const shoes = [
+    { name: "Birkenstocks", price: 50, type: "sandal" },
+    { name: "Air Jordans", price: 500, type: "sneaker" },
+    { name: "Air Mahomeses", price: 501, type: "sneaker" },
+    { name: "Utility Boots", price: 20, type: "boot" },
+    { name: "Velcro Sandals", price: 15, type: "sandal" },
+    { name: "Jet Boots", price: 1000, type: "boot" },
+    { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+];
+
+app.get("/shoes", (req, res) => {
+    const minPrice = req.query["min-price"];
+    const maxPrice = req.query["max-price"];
+    const type = req.query.type;
+
+    let result = shoes;
+    if (minPrice) {
+        result = result.filter(function (shoe) {
+            return shoe.price >= Number(minPrice);
+        });
+    }
+    if (maxPrice) {
+        result = result.filter(function (shoe) {
+            return shoe.price <= Number(maxPrice);
+        });
+    }
+    if (type) {
+        result = result.filter(function (shoe) {
+            return shoe.type === type;
+        });
+    }
+
+    res.send(result);
+});
+
